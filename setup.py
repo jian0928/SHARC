81a2c2626679e62841e1e9cd75e169026a00552b
jian <62329745+jian0928@users.noreply.github.com>
2026-09-20 17:46:02 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sharc',
+    version='0.1.0',
+    description='Single-image 3D human reconstruction',
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=[],
+)
